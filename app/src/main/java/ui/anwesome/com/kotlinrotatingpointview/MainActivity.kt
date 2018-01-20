f1ea7f76626ad3405b5ae8ed63286c55f9914fac
Anwesh43 <anweshthecool0@gmail.com>
2018-01-20 17:55:55 +0530

    adding RotationCompleteListener

--- app/src/main/java/ui/anwesome/com/kotlinrotatingpointview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinrotatingpointview/MainActivity.kt
@@ -3,13 +3,17 @@ package ui.anwesome.com.kotlinrotatingpointview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import ui.anwesome.com.rotatingpointview.RotatingPointView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RotatingPointView.create(this)
+        var view = RotatingPointView.create(this)
+        view.addRotationCompleteListener {
+            Toast.makeText(this,"Rotation Complete",Toast.LENGTH_SHORT).show()
+        }
         fullScreen()
     }
 }
