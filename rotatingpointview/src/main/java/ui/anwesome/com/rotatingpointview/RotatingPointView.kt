835a36ef3cb109c9b23e54ac8a8b5e659569a790
Anwesh43 <anweshthecool0@gmail.com>
2018-01-20 06:42:10 +0530

    adding RotatingPointView

--- rotatingpointview/src/main/java/ui/anwesome/com/rotatingpointview/RotatingPointView.kt
+++ rotatingpointview/src/main/java/ui/anwesome/com/rotatingpointview/RotatingPointView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.rotatingpointview
 /**
  * Created by anweshmishra on 20/01/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class RotatingPointView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
