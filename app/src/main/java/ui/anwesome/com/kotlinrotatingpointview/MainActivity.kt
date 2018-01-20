cf2a6de77b1a888da64710a9064c1692ed3f8e6d
Anwesh43 <anweshthecool0@gmail.com>
2018-01-20 17:13:16 +0530

    adding RotatingPointView in activity

--- app/src/main/java/ui/anwesome/com/kotlinrotatingpointview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinrotatingpointview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinrotatingpointview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.rotatingpointview.RotatingPointView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotatingPointView.create(this)
     }
 }
